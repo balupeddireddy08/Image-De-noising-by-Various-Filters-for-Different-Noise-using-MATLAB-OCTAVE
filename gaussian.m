5249cf2e1d0ca2dac1b803a744958aae5eb6d466
balupeddireddy08 <59537760+balupeddireddy08@users.noreply.github.com>
2020-05-31 21:14:57 +0530

    Add files via upload

---
+++ gaussian.m
@@ -0,0 +1,21 @@
+%Filtering of an image having Gaussian noise
+f=imread('C:\Users\Balu\Desktop\dsp_project\flower.gif');
+subplot(2,3,1)
+imshow(f),title('Original image');
+g=imnoise(f,'gaussian',0,0.05);
+subplot(2,3,2)
+imshow(g),title('Noisy image')
+h = fspecial('unsharp');
+m=fspecial('average',[3,3]);
+j=imfilter(g,h);
+subplot(2,3,3)
+imshow(j),title(' Linear filtered image')
+k=filter2(fspecial('average',3),g)/255;
+subplot(2,3,4)
+imshow(k),title('FIR filtered image')
+l = medfilt2(g,[3 3]);
+subplot(2,3,5)
+imshow(l),title('Median filtered image')
+m= wiener2(g,[5 5]);
+subplot(2,3,6)
+imshow(m),title('Adaptive filtered image') 
